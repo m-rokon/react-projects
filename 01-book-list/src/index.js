e945100a94efab6982bf676ce4aca4550f97b395
m-rokon <ratertara.rokon@gmail.com>
2025-10-04 20:47:03 +0600

    skeleton done for the app

--- 01-book-list/src/index.js
+++ 01-book-list/src/index.js
@@ -2,11 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Booklist = () => {
-	return <section></section>;
+	return (
+		<section>
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
 };
 
 const Book = () => {
-	return <article></article>;
+	return (
+		<article>
+			<Image />
+			<Title />
+			<Description />
+		</article>
+	);
+};
+
+const Image = () => {
+	return <h2>this will replaced by image container</h2>;
+};
+
+const Title = () => {
+	return <h2>Book title</h2>;
+};
+
+const Description = () => {
+	return (
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, eveniet?
+		</p>
+	);
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
