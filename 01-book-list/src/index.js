e2fba217087a6913c2545173f64d96c6d69ed5d2
m-rokon <ratertara.rokon@gmail.com>
2025-10-05 00:48:01 +0600

    cleanup code

--- 01-book-list/src/index.js
+++ 01-book-list/src/index.js
@@ -14,35 +14,19 @@ const Booklist = () => {
 };
 
 const Book = () => {
+	const title =
+		"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About";
+	const author = "Mel Robbins";
 	return (
 		<article className="book">
-			<Image />
-			<Title />
-			<Author />
+			<img
+				src="https://m.media-amazon.com/images/I/91ZVf3kNrcL._SY466_.jpg"
+				alt="The Let Them Theory"
+			/>
+			<h2>{title}</h2>
+			<h4>{author}</h4>
 		</article>
 	);
-};
-
-const Image = () => {
-	return (
-		<img
-			src="https://m.media-amazon.com/images/I/91ZVf3kNrcL._SY466_.jpg"
-			alt="The Let Them Theory"
-		/>
-	);
-};
-
-const Title = () => {
-	return (
-		<h2>
-			The Let Them Theory: A Life-Changing Tool That Millions of People Can't
-			Stop Talking About
-		</h2>
-	);
-};
-
-const Author = () => {
-	return <h4>Mel Robbins</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
