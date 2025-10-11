c1c61017b23f5faebbe85b78728616a0c73ef792
m-rokon <ratertara.rokon@gmail.com>
2025-10-12 01:04:55 +0600

    somewhat dynamic `props`

--- 01-book-list/src/index.js
+++ 01-book-list/src/index.js
@@ -3,16 +3,32 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const title =
-	"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About";
-const author = "Mel Robbins";
-const img = "./images/book-1.jpg";
+// somewhat dynamic props
+const firstBook = {
+	title:
+		"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About",
+	author: "Mel Robbins",
+	img: "./images/book-1.jpg",
+};
+const secondBook = {
+	title: "How to Test Negative for Stupid: And Why Washington Never Will",
+	author: "John Kennedy",
+	img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SY342_.jpg",
+};
 
 const Booklist = () => {
 	return (
 		<section className="booklist">
-			<Book title={title} author={author} img={img} />
-			<Book title={title} author={author} img={img} />
+			<Book
+				title={firstBook.title}
+				author={firstBook.author}
+				img={firstBook.img}
+			/>
+			<Book
+				title={secondBook.title}
+				author={secondBook.author}
+				img={secondBook.img}
+			/>
 		</section>
 	);
 };
