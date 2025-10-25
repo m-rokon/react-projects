3abd13659b64ed6730f72ad07531b77f20054ef6
m-rokon <ratertara.rokon@gmail.com>
2025-10-26 00:22:49 +0600

    proper list

--- 01-book-list/src/index.js
+++ 01-book-list/src/index.js
@@ -3,52 +3,42 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-// somewhat dynamic props
-const firstBook = {
-	title:
-		"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About",
-	author: "Mel Robbins",
-	img: "./images/book-1.jpg",
-};
-const secondBook = {
-	title: "How to Test Negative for Stupid: And Why Washington Never Will",
-	author: "John Kennedy",
-	img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SY342_.jpg",
-};
+// proper list
+
+const books = [
+	{
+		title:
+			"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About",
+		author: "Mel Robbins",
+		img: "./images/book-1.jpg",
+	},
+	{
+		title: "How to Test Negative for Stupid: And Why Washington Never Will",
+		author: "John Kennedy",
+		img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SY342_.jpg",
+	},
+];
 
 const Booklist = () => {
 	return (
 		<section className="booklist">
-			<Book
-				title={firstBook.title}
-				author={firstBook.author}
-				img={firstBook.img}
-			>
-				{/* children props */}
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officia
-					ut consectetur! Amet deserunt pariatur aliquam at illum vel sit.
-				</p>
-				<button>Click Me!</button>
-			</Book>
-			<Book
-				title={secondBook.title}
-				author={secondBook.author}
-				img={secondBook.img}
-			/>
+			{books.map((book) => {
+				const { author, title, img } = book;
+				return <Book title={title} img={img} author={author} />;
+			})}
 		</section>
 	);
 };
 
 // destructuring props
 
-const Book = ({ author, title, img, children }) => {
+const Book = (props) => {
+	const { author, title, img } = props;
 	return (
 		<article className="book">
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
 			<h4>{author}</h4>
-			{children}
 		</article>
 	);
 };
