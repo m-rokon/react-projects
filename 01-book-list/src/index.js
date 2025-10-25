dda5299fda33b6865ded7b7537805356f16c24b8
m-rokon <ratertara.rokon@gmail.com>
2025-10-26 00:57:48 +0600

    `key` prop

--- 01-book-list/src/index.js
+++ 01-book-list/src/index.js
@@ -11,11 +11,13 @@ const books = [
 			"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About",
 		author: "Mel Robbins",
 		img: "./images/book-1.jpg",
+		id: 1,
 	},
 	{
 		title: "How to Test Negative for Stupid: And Why Washington Never Will",
 		author: "John Kennedy",
 		img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SY342_.jpg",
+		id: 2,
 	},
 ];
 
@@ -23,8 +25,8 @@ const Booklist = () => {
 	return (
 		<section className="booklist">
 			{books.map((book) => {
-				const { author, title, img } = book;
-				return <Book title={title} img={img} author={author} />;
+				const { author, title, img, id } = book;
+				return <Book title={title} img={img} author={author} key={id} />;
 			})}
 		</section>
 	);
