f23ccf9905843796822f992f978f78cbe343aa41
m-rokon <ratertara.rokon@gmail.com>
2025-11-08 01:32:32 +0600

    local images folder

--- 01-book-list/src/books.js
+++ 01-book-list/src/books.js
@@ -1,15 +1,25 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
 export const books = [
 	{
 		title:
 			"The Let Them Theory: A Life-Changing Tool That Millions of People Can't Stop Talking About",
 		author: "Mel Robbins",
-		img: "./images/book-1.jpg",
+		img: img1,
 		id: 1,
 	},
 	{
 		title: "How to Test Negative for Stupid: And Why Washington Never Will",
 		author: "John Kennedy",
-		img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SY342_.jpg",
+		img: img2,
 		id: 2,
+	},
+	{
+		title: "Notes on Being a Man",
+		author: "Scott Galloway",
+		img: img3,
+		id: 3,
 	},
 ];
