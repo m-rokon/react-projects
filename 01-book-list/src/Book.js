4b94e732ad5a8afef863a2405030087a30d7f143
m-rokon <ratertara.rokon@gmail.com>
2025-11-08 23:42:31 +0600

    numbers challenge - css moved into index.css file

--- 01-book-list/src/Book.js
+++ 01-book-list/src/Book.js
@@ -2,19 +2,8 @@ const Book = (props) => {
 	const { author, title, img, numbers } = props;
 	// console.log(displayBook);
 	return (
-		<article style={{ position: "relative" }} className="book">
-			<span
-				style={{
-					position: "absolute",
-					top: "0px",
-					left: "0px",
-					background: "#ff4000ff",
-					color: "#fff",
-					padding: "0.2rem 1rem",
-				}}
-			>
-				#{numbers + 1}
-			</span>
+		<article className="book">
+			<span className="number">#{numbers + 1}</span>
 			<img src={img} alt={title} />
 			<h2>{title}</h2>
 			<h4>{author}</h4>
