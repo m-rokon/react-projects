67fb92b12e5bb26706325e6bb6db9336cf0a49e2
m-rokon <ratertara.rokon@gmail.com>
2025-11-12 15:04:46 +0600

    backroad init - final

--- 02-backroads-project/src/App.js
+++ 02-backroads-project/src/App.js
@@ -1,22 +1,8 @@
-import React from 'react';
-import About from './components/About';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Services from './components/Services';
-import Tours from './components/Tours';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <Tours />
-      <Footer />
-    </React.Fragment>
-  );
+	return <h2>Hello React</h2>;
 }
 
 export default App;
