385d2ce4d34f40e0b99d37bdb6436211ee7a10e5
m-rokon <ratertara.rokon@gmail.com>
2025-11-20 23:01:56 +0600

    `tours` and data.js update

--- 02-backroads-project/src/data.js
+++ 02-backroads-project/src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
 	{
 		id: 1,
@@ -58,5 +63,53 @@ export const services = [
 		title: "amazing comfort",
 		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
 		icon: "fas fa-socks fa-fw",
+	},
+];
+
+// tours
+export const tours = [
+	{
+		id: 1,
+		img: tour1,
+		date: "august 26th, 2020",
+		title: "Tibet Adventure",
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		country: "china",
+		tripTime: "6 days",
+		price: "from $2100",
+	},
+	{
+		id: 2,
+		img: tour2,
+		date: "october 1st, 2020",
+		title: "best of java",
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		country: "indonesia",
+		tripTime: "11 days",
+		price: "from $1400",
+	},
+	{
+		id: 3,
+		img: tour3,
+		date: "september 15th, 2020",
+		title: "explore hong kong",
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		country: "hong kong",
+		tripTime: "8 days",
+		price: "from $5000",
+	},
+	{
+		id: 4,
+		img: tour4,
+		date: "december 5th, 2020",
+		title: "kenya highlights",
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+		icon: "fas fa-map",
+		country: "kenya",
+		tripTime: "20 days",
+		price: "from $3300",
 	},
 ];
