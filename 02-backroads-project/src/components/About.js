3378ed0f996bf095da1ba2b2c2864d6541b6510f
m-rokon <ratertara.rokon@gmail.com>
2025-11-20 18:48:49 +0600

    `About` component fix

--- 02-backroads-project/src/components/About.js
+++ 02-backroads-project/src/components/About.js
@@ -1,21 +1,18 @@
+import aboutImage from "../images/about.jpeg";
 const About = () => {
 	return (
-		<section class="section" id="about">
-			<div class="section-title">
+		<section className="section" id="about">
+			<div className="section-title">
 				<h2>
 					about <span>us</span>
 				</h2>
 			</div>
 
-			<div class="section-center about-center">
-				<div class="about-img">
-					<img
-						src="./images/about.jpeg"
-						class="about-photo"
-						alt="awesome beach"
-					/>
+			<div className="section-center about-center">
+				<div className="about-img">
+					<img src={aboutImage} className="about-photo" alt="awesome beach" />
 				</div>
-				<article class="about-info">
+				<article className="about-info">
 					<h3>explore the difference</h3>
 					<p>
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
@@ -27,7 +24,7 @@ const About = () => {
 						quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
 						unde dolor?
 					</p>
-					<a href="#" class="btn">
+					<a href="#" className="btn">
 						read more
 					</a>
 				</article>
