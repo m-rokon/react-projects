49bcef2a7ff207639d7a335f920ad06ada828e15
m-rokon <ratertara.rokon@gmail.com>
2025-12-05 21:49:49 +0600

    data fetch from `data.js`

--- 03-birthday-buddy/src/App.jsx
+++ 03-birthday-buddy/src/App.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import data from "./data";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+	const [birthdayBuddy, setBirthdayBuddy] = useState(data);
+	return (
+		<section>
+			<article>
+				{birthdayBuddy.map((people) => {
+					const { id, name, age, image } = people;
+					return (
+						<div key={id}>
+							<div>
+								<img src={image} alt={name} />
+							</div>
+							<div>
+								<h4>{name}</h4>
+								<p>{age}</p>
+							</div>
+						</div>
+					);
+				})}
+			</article>
+		</section>
+	);
 };
 export default App;
